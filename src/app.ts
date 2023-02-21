991786783171f2829923079e69b4e23af8bf6c16
Yacinn <sassou.est100@gmail.com>
2023-02-21 22:53:50 +0100

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,24 @@
 import { exec } from "child_process";
-
-// exec("typeorm-model-generator -h localhost/orcl -d HR -p 1521 -u HR -x root -e oracle", (error, stdout, stderr) => {
-//     if (error) {
-//         console.log(`error: ${error.message}`);
-//         return;
-//     }
-//     if (stderr) {
-//         console.log(`stderr: ${stderr}`);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout}`);
-// })
-
-import oracledb from "oracledb";
+import dotenv from "dotenv";
+dotenv.config();
 
 
-async function fun() {
-    const con = await oracledb.getConnection({
-        user: "HR",
-        password: "root",
-        connectString: "localhost/orcl"
-    });
-    const result = await con.execute("select * from employees");
-    console.log(result);
-}
 
-fun();
+exec(`typeorm-model-generator -h ${process.env.DB_HOST} -d ${process.env.DB_NAME} -p ${process.env.DB_PORT} -u ${process.env.DB_USERNAME} -x ${process.env.DB_PASSWORD} -e oracle`, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+})
+
+
+import scrape from "./scrapeForEntities";
+(async () => {
+    let entities = await scrape("./output/entities");
+    console.log(entities);
+})();
